f38b0c2306c4b7d5e5cae539edc9a3c53a613dfb
ldalomoto <ldalomoto@uce.edu.ec>
2026-01-29 23:30:25 -0500

    aws

--- UniDream-Interfaz/src/services/api.ts
+++ UniDream-Interfaz/src/services/api.ts
@@ -1,40 +1,29 @@
 // src/services/api.ts
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/docs#"; // URL local de FastAPI
+export const API_URL = "http://3.144.209.174";// URL local de FastAPI
 
 export const chat = async (userId: string, message: string) => {
   const response = await axios.post(`${API_URL}/chat/stream`, { user_id: userId, message });
   return response.data;
 };
 
-export const chatStream = async (
-  userId: string,
-  message: string,
-  onChunk: (chunk: string) => void
-) => {
-  const response = await fetch("http://localhost:8000/chat/stream", {
+export const chatStream = async (userId: string, message: string, onChunk: (chunk: string) => void) => {
+  const response = await fetch(`${API_URL}/chat/stream`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      user_id: userId,
-      message
-    })
+    body: JSON.stringify({ user_id: userId, message })
   });
-
-  if (!response.ok) {
-    throw new Error("Error en la conexión con el backend");
-  }
 
   const reader = response.body?.getReader();
   const decoder = new TextDecoder();
-
   if (!reader) return;
 
   while (true) {
     const { value, done } = await reader.read();
     if (done) break;
 
-    onChunk(decoder.decode(value));
+    const chunk = decoder.decode(value);
+    onChunk(chunk);
   }
 };
